90bdb24e383b41f158d7d8b5b6527fe925b846f6
RyanShereda <39539590+RyanShereda@users.noreply.github.com>
2020-04-21 17:03:48 -0500

    adds rpsProject files (pseudocode)

---
+++ rpsProject/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace rpsProject
+{
+    //create enumerated list for each possible choice
+    //{rock, paper, scissors}
+
+    class Program
+    {
+        //number tie count
+
+        static void Main(string[] args)
+        {
+            //create player 1 object
+            //create player 2 object
+
+            //set player 1 name (readLine)
+            //set player 2 name (readLine)
+
+            //repeat main gameplay method until one wins
+            //Do while loop {
+                //run main gameplay method
+            //} ends when one player has more than 2 points
+
+            //if player 1 score more than 2
+                //write to console "[player1] wins [player1.score]-[player2.score] with [tie count] ties"
+            //} else if player 2 score more than 2 {
+                //write to console "[player2] wins [player2.score]-[player1.score] with [tie count] ties"
+            //}
+        }
+
+        //main gameplay method(round # parameter){
+
+            //variable to hold winner
+
+            //player 1 gets random enum/number
+            //player 2 gets random enum/number
+
+        //}
+
+        //if statement checking player 1's choice
+        //if (player1.value is rock) {
+            //nested if statement checking player 2's choice
+            //if (player2.choice is paper) {
+                //player 2 wins
+                //winner = player2
+                //player2.score + 1;
+            //} else if (player2.choice is scissors) {
+                //player 1 wins
+                //winner = player1
+                //player1.score + 1;
+            //}
+        //} else if (player1.value is paper) {
+            //nested if statement checking player 2's choice
+            //if (player2.choice is scissors) {
+                //player 2 wins
+                //winner = player2
+                //player2.score + 1;
+            //} else if (player2.choice is rock) {
+                //player 1 wins
+                //winner = player1
+                //player1.score + 1;
+            //}
+        //} else if (player1.value is scissors) {
+            //nested if statement checking player 2's choice
+            //if (player2.choice is rock) {
+                //player 2 wins
+                //winner = player2
+                //player2.score + 1;
+            //} else if (player2.choice is paper) {
+                //player 1 wins
+                //winner = player1
+                //player1.score + 1;
+            //}
+        //}
+
+        //check winner ([IF] there is a tie)
+            //write there was a tie
+            //tie count + 1
+        //[ELSE] a player won
+            //add score out here instead?
+            //write results to console
+            //round # - [player1] had [value], [player2] had [value] - [winner] won
+        //[END IF]
+    //}
+    
+
+    //small class to hold player names/scores {
+        //player name variable holder
+        //player score variable holder
+
+        //initialize player variables(name) method
+    //}
+
+    }
+}
