a3cc6730361e8558e038e990fbfc1fb5f9eb3389
RyanShereda <39539590+RyanShereda@users.noreply.github.com>
2020-04-23 18:30:57 -0500

    Adds project used for trying out lambda functions and delegates.

---
+++ Day4Experiments/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day4Experiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Lambda Counter
+            int counter = 0;
+
+            //console formatting
+            System.Console.WriteLine("-------------------------------");
+            System.Console.WriteLine($"Current value of the counter: {counter}");
+
+            Action<int> counterUp = value => 
+            {
+                //lambda function writes the action, then performs it
+                System.Console.WriteLine($"Increasing counter by {value}");   
+                counter += value;
+            };
+
+            //loop to run process multiple times, for display of versatility
+            for (int i = 1; i <= 3; i++) {
+                counterUp(i);
+                System.Console.WriteLine($"Current value of the counter: {counter}");
+            }
+
+            //end of program console formatting
+            System.Console.WriteLine("-------------------------------");
+            #endregion
+        }
+    }
+}
