5da26af20b8ef3d0c7550bf10ae044f8b6c8ee77
RyanShereda <39539590+RyanShereda@users.noreply.github.com>
2020-04-29 23:03:17 -0500

    Adds completed constructors for all models.

--- Project0/BitsAndBobs/BitsAndBobs/Models/OrderLineItem.cs
+++ Project0/BitsAndBobs/BitsAndBobs/Models/OrderLineItem.cs
@@ -6,5 +6,40 @@ namespace BitsAndBobs.Models
 {
     public class OrderLineItem
     {
-    }
+        public int OrderLineItemID { get; set; }
+
+		private int orderID;
+
+		public int OrderID
+		{
+			get { return orderID; }
+			set { orderID = value; }
+		}
+
+		private int productID;
+
+		public int ProductID
+		{
+			get { return productID; }
+			set { productID = value; }
+		}
+
+		private int unitPrice;
+
+		public int UnitPrice
+		{
+			get { return unitPrice; }
+			set { unitPrice = value; }
+		}
+
+		private int quantity;
+
+		public int Quantity
+		{
+			get { return quantity; }
+			set { quantity = value; }
+		}
+
+		public OrderLineItem() { }
+	}
 }
