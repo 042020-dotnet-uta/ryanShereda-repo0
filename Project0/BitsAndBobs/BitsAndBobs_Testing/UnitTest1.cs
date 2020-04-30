cb9b8a5e42b7aa5cebf0a492e4bcf5f21f30157d
RyanShereda <39539590+RyanShereda@users.noreply.github.com>
2020-04-30 08:47:24 -0500

    Adds spaces for remaining minimum test cases to XUnit testing file.

--- Project0/BitsAndBobs/BitsAndBobs_Testing/UnitTest1.cs
+++ Project0/BitsAndBobs/BitsAndBobs_Testing/UnitTest1.cs
@@ -8,10 +8,11 @@ namespace BitsAndBobs_Testing
     public class UnitTest1
     {
         /// <summary>
+        /// Test 1 -- 
         /// This test tries searching for an existing Customer object, ensuring it was perpetuated it to the database.
         /// </summary>
         [Fact]
-        public void Test1()
+        public void TestCustomerSearch()
         {
             //Arrange
             var options = new DbContextOptionsBuilder<BaB_DbContext>()
@@ -38,7 +39,195 @@ namespace BitsAndBobs_Testing
                 var testCustomerName = context.Customers.Where(c => c.CustomerID == 1).FirstOrDefault();
                 Assert.Equal("Annie", testCustomerName.CustFirstName);
             }
+        }
 
+        
+        [Fact]
+        public void Test2()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<BaB_DbContext>()
+                .UseInMemoryDatabase(databaseName: "CustomNameForThisTestsInMemoryDb")
+                .Options;
+
+            //Act
+            using (var context = new BaB_DbContext(options))
+            {
+                
+            }
+
+            //Assert
+            using (var context = new BaB_DbContext(options))
+            {
+               
+            }
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<BaB_DbContext>()
+                .UseInMemoryDatabase(databaseName: "CustomNameForThisTestsInMemoryDb")
+                .Options;
+
+            //Act
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+
+            //Assert
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<BaB_DbContext>()
+                .UseInMemoryDatabase(databaseName: "CustomNameForThisTestsInMemoryDb")
+                .Options;
+
+            //Act
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+
+            //Assert
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<BaB_DbContext>()
+                .UseInMemoryDatabase(databaseName: "CustomNameForThisTestsInMemoryDb")
+                .Options;
+
+            //Act
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+
+            //Assert
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<BaB_DbContext>()
+                .UseInMemoryDatabase(databaseName: "CustomNameForThisTestsInMemoryDb")
+                .Options;
+
+            //Act
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+
+            //Assert
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<BaB_DbContext>()
+                .UseInMemoryDatabase(databaseName: "CustomNameForThisTestsInMemoryDb")
+                .Options;
+
+            //Act
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+
+            //Assert
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<BaB_DbContext>()
+                .UseInMemoryDatabase(databaseName: "CustomNameForThisTestsInMemoryDb")
+                .Options;
+
+            //Act
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+
+            //Assert
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+        }
+        [Fact]
+        public void Test9()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<BaB_DbContext>()
+                .UseInMemoryDatabase(databaseName: "CustomNameForThisTestsInMemoryDb")
+                .Options;
+
+            //Act
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+
+            //Assert
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+        }
+
+        [Fact]
+        public void Test10()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<BaB_DbContext>()
+                .UseInMemoryDatabase(databaseName: "CustomNameForThisTestsInMemoryDb")
+                .Options;
+
+            //Act
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
+
+            //Assert
+            using (var context = new BaB_DbContext(options))
+            {
+
+            }
         }
     }
 }
