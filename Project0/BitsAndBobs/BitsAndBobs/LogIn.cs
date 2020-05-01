af143d97eef86da14b81d97bdd602e34e8ef16f3
RyanShereda <39539590+RyanShereda@users.noreply.github.com>
2020-05-01 00:48:55 -0500

    Adds log in code up to database requirements; need to establish database before continuing. Maybe not, but it's 1am. Go to sleep Ryan.

--- Project0/BitsAndBobs/BitsAndBobs/LogIn.cs
+++ Project0/BitsAndBobs/BitsAndBobs/LogIn.cs
@@ -24,6 +24,9 @@ namespace BitsAndBobs
             get { return loggedInCustomerID; }
         }
 
+        //initialize variable to hold user input in this class
+        String userInput = "";
+
 
         /// <summary>
         /// This method welcomes the user, then calls the main method of the class with the LogIn input reader
@@ -33,6 +36,9 @@ namespace BitsAndBobs
             //print statement welcoming user
             Console.WriteLine("Welcome!");
 
+            //Re-initialize the logged in customer's ID to -1, in case of returning to this method!
+            loggedInCustomerID = -1;
+            
             //call LogInStart method, implementing a new User Input "device"
             LogInStart(new UserInputLogIn());
         }
@@ -43,10 +49,7 @@ namespace BitsAndBobs
         /// <param name="input">The input reader to be used for this method.</param>
         public void LogInStart(IUserInput input)
         {
-            //initialize variable to hold user input in this class
-            var userInput = "";
-
-            while (true)
+            while (LoggedInCustomerID == -1)
             {
                 //Ask if they would like to "Sign in" or "Create an account"
                 Console.WriteLine("Please enter \"Log in\" to log in to an existing account, \"Sign up\" to create a new account, or \"Exit\" to exit the application.");
@@ -55,12 +58,12 @@ namespace BitsAndBobs
                 if ((userInput.ToLower() == "log in") || (userInput.ToLower() == "login"))
                 {
                     LogInExistingUser(input);
-                    break;
+                    
                 }
                 else if (userInput.ToLower() == "sign up" || (userInput.ToLower() == "signup"))
                 {
                     //GO TO CREATE NEW CUSTOMER METHOD
-                    break;
+                    
                 }
                 else if (userInput.ToLower() == "exit")
                 {
@@ -75,7 +78,16 @@ namespace BitsAndBobs
 
         public void LogInExistingUser(IUserInput input)
         {
-            Console.WriteLine("Logging in existing user!");
+            Console.WriteLine("Welcome back.");
+            Console.Write("Please enter your username: ");
+            userInput = input.GetInput();
+            //Need to query database now--get second set of eyes looking into why database doesn't exist
+            //query database for custUsername == userInput, store password somewhere
+            //if username found, request password
+            //if password matches, set loggedInUserID = customerID
+            //else if username not found or password does not match
+            //"Invalid username/password. Please try again, or "sign up" to create a new account."
+            //loop until valid credentials or transfer out of loop
         }
 
         public void CreateNewCustomer(IUserInput input)
