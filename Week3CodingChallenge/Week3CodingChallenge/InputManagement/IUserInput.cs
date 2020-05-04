bc296f7cd7955508508a09d84711475198266a20
RyanShereda <39539590+RyanShereda@users.noreply.github.com>
2020-05-04 15:12:43 -0500

    Creates and completes the coding challenge for week 3.

---
+++ Week3CodingChallenge/Week3CodingChallenge/InputManagement/IUserInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Code snippet taken from my p0, BitsAndBobs
+//Guidelines for this method of dependency injection was taken from
+//https://stackoverflow.com/questions/10181421/how-do-i-apply-unit-testing-to-c-sharp-function-which-requires-user-input-dynami
+namespace Week3CodingChallenge.InputManagement
+{
+    public interface IUserInput
+    {
+        public string GetInput();
+    }
+}
---
+++ Week3CodingChallenge/Week3CodingChallenge/InputManagement/UserInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Code snippet taken from my p0, BitsAndBobs
+//Guidelines for this method of dependency injection was taken from
+//https://stackoverflow.com/questions/10181421/how-do-i-apply-unit-testing-to-c-sharp-function-which-requires-user-input-dynami
+namespace Week3CodingChallenge.InputManagement
+{
+    //read input from user
+    public class UserInput : IUserInput
+    {
+        public string GetInput()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
